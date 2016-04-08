ff02f2c3710a464b7ace2e1d57c1c01483924f90
rappinkraken <thenj@ymail.com>
2016-04-07 20:43:47 -0400

    Adding Login Verification

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,8 +1,14 @@
 package controllers;
+import java.util.HashMap;
+import java.util.Map;
+
 import  models.User; 
 import play.mvc.Controller;
 import play.mvc.Http.Request;
 import play.mvc.Result;
+
+import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.*;
 
 public class Application extends Controller {
@@ -10,32 +16,65 @@ public class Application extends Controller {
 	
 	
     public static Result index() {
-        return ok(views.html.index.render("Confusion"));
+        return ok(views.html.index.render("Confusion Inventory"));
     }
     
     public static Result submit() {
-    	return ok(views.html.submit.render("Balls"));
-    	}
-    public static Result myAction() {
-    String[] postAction = request().body().asFormUrlEncoded().get("action");
-    if (postAction == null || postAction.length == 0) {
-      return badRequest("You must provide a valid action");
-    }
-    else {
-      String action = postAction[0];
-      if ("submit".equals(action)) {
-        return edit(request());
-      } 
-      else {
-        return badRequest("Something Bad Happened :(");
-      }
-    }
-  }
+    	
+    	User example = new User();
+    	
+    	
+    	 DynamicForm userForm = Form.form().bindFromRequest();
+         String username = userForm.data().get("username");
+         String password = userForm.data().get("password");
+         
+         example.setUsername(username);
+         example.setPassword(password);
+         
+         
+//         Map<String, String> credentials = new HashMap<String, String>();
+//
+// 	    credentials.put("USER", username );
+// 	    credentials.put("PASS", password);
+ 	    
+ 	   
 
-  private static Result edit(Request request) {
-    // TODO
-    return submit();
-  }
+    	if (username == "" ){
+    		 flash("error", "Missing Username");
+             return ok(views.html.index.render("Enter a Username!"));
+    	}
+    	else if (password == "" ){
+    		flash("error", "Missing Password");
+            return ok(views.html.index.render("Enter a Password!"));
+    	}
+    	
+    		return ok(views.html.submit.render(example));
+    	}
+    
+    
+
+    
+    
+//    public static Result myAction() {
+//    String[] postAction = request().body().asFormUrlEncoded().get("action");
+//    if (postAction == null || postAction.length == 0) {
+//      return badRequest("You must provide a valid action");
+//    }
+//    else {
+//      String action = postAction[0];
+//      if ("submit".equals(action)) {
+//        return edit(request());
+//      } 
+//      else {
+//        return badRequest("Something Bad Happened :(");
+//      }
+//    }
+//  }
+//
+//  private static Result edit(Request request) {
+//    // TODO
+//    return submit();
+//  }
 
   
 }
