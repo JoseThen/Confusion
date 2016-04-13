4172e719d437ad44cb2827b9b2cf6490110fd724
rappinkraken <thenj@ymail.com>
2016-04-13 18:35:14 -0400

    Add Employee actual name to submit page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -50,11 +50,14 @@ public class Application extends Controller {
             flash("error", "Incorrect username or password.");
             return ok(views.html.index.render("Incorrect username or password."));
         }
-
+        
         User example = new User();
-        example.setUsername(username);
-        example.setPassword(password);
+        example.set_EmployeeName(username) ;
+   
+        
+       
          
+        // We might be able to use a map for search functionality
 //         Map<String, String> credentials = new HashMap<String, String>();
 //
 // 	    credentials.put("USER", username );
@@ -67,26 +70,6 @@ public class Application extends Controller {
 
     
     
-//    public static Result myAction() {
-//    String[] postAction = request().body().asFormUrlEncoded().get("action");
-//    if (postAction == null || postAction.length == 0) {
-//      return badRequest("You must provide a valid action");
-//    }
-//    else {
-//      String action = postAction[0];
-//      if ("submit".equals(action)) {
-//        return edit(request());
-//      } 
-//      else {
-//        return badRequest("Something Bad Happened :(");
-//      }
-//    }
-//  }
-//
-//  private static Result edit(Request request) {
-//    // TODO
-//    return submit();
-//  }
 
 
     private static boolean isLogin(Statement st, String userName, String password) throws SQLException {
--- app/models/User.java
+++ app/models/User.java
@@ -1,4 +1,9 @@
 package models;
+import java.sql.*;
+
+import javax.sql.DataSource;
+
+import play.db.DB;
 
 
 
@@ -6,6 +11,7 @@ package models;
 public class User {
 	public String Username;
 	public String Password;
+	public String EmployeeName;
 	
 	public void setUsername(String username) {
         this.Username = username;
@@ -21,6 +27,33 @@ public class User {
 
     public String getPassword() {
         return Password;
+    }
+    
+    public void set_EmployeeName(String empname){
+    	
+    	String first = "SELECT first_name, last_name FROM employee where user_name ='" + empname + "'";
+    	String realName = null;
+    	
+         try {
+             DataSource dsource = DB.getDataSource();
+             Connection connecion = dsource.getConnection();
+             Statement state = connecion.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+             ResultSet welcomer = state.executeQuery(first);
+            	while (welcomer.next()){
+            		if (welcomer !=  null){
+            			realName = welcomer.getString("first_name") + " " + welcomer.getString("last_name");
+            		}
+            	}
+            	
+            		
+            	
+            connecion.close();
+         } catch (SQLException e) {
+             e.printStackTrace();
+         	}
+
+         
+         this.Username = realName;
     }
 
 }
