c581444eaa55d27850dd52bb34024fba864ed3d8
Maryia.Piletskaya <hey.november@gmail.com>
2016-04-18 10:28:16 -0400

    Creating additional classes

---
+++ app/models/Product.java
@@ -0,0 +1,74 @@
+package models;
+
+public class Product {
+
+    private String name;
+    private String description;
+    private float price;
+    private String version;
+    private int amount;
+    private Category category;
+    private Publisher publisher;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+
+
+
+
+
+}
---
+++ app/models/Publisher.java
@@ -0,0 +1,25 @@
+package models;
+
+public class Publisher {
+
+
+    private String name;
+    private String country;
+
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
